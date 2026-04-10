5469717ba6869738202e88c252cd59525c708106
Julia Siqueira <jusiqueira@davidson.edu>
2026-04-10 12:52:24 -0400

    Added Avatar.jsx back w/ no errors as long as image-picker and avatar mentions were commented out

---
+++ components/Avatar.jsx
@@ -0,0 +1,113 @@
+import { useState, useEffect } from 'react'
+import { supabase } from '../supabase'
+import { StyleSheet, View, Alert, Image, Button } from 'react-native'
+/* import * as ImagePicker from 'expo-image-picker' */ 
+
+export default function Avatar({ url, size = 150, onUpload }) {
+  const [uploading, setUploading] = useState(false)
+  const [avatarUrl, setAvatarUrl] = useState(null)
+  const avatarSize = { height: size, width: size }
+
+  useEffect(() => {
+    if (url) downloadImage(url)
+  }, [url])
+
+  async function downloadImage(path) {
+    try {
+      const { data, error } = await supabase.storage.from('avatars').download(path)
+
+      if (error) {
+        throw error
+      }
+
+      const fr = new FileReader()
+      fr.readAsDataURL(data)
+      fr.onload = () => {
+        setAvatarUrl(fr.result)
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log('Error downloading image: ', error.message)
+      }
+    }
+  }
+
+  async function uploadAvatar() {
+    try {
+      setUploading(true)
+
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ['images'],
+        allowsEditing: true,
+        quality: 1,
+      })
+
+      if (result.canceled || !result.assets || result.assets.length === 0) {
+        return
+      }
+
+      const image = result.assets[0]
+      if (!image.uri) {
+        throw new Error('No image uri!')
+      }
+
+      const arraybuffer = await fetch(image.uri).then((res) => res.arrayBuffer())
+      const fileExt = image.uri.split('.').pop()?.toLowerCase() ?? 'jpeg'
+      const filePath = `${Math.random()}.${fileExt}`
+
+      const { error } = await supabase.storage.from('avatars').upload(filePath, arraybuffer, {
+        contentType: image.mimeType ?? 'image/jpeg',
+      })
+
+      if (error) {
+        throw error
+      }
+
+      onUpload(filePath)
+    } catch (error) {
+      if (error instanceof Error) {
+        Alert.alert(error.message)
+      }
+    } finally {
+      setUploading(false)
+    }
+  }
+
+ /* 
+  return (
+    <View>
+      {avatarUrl ? (
+        <Image
+          source={{ uri: avatarUrl }}
+          accessibilityLabel="Avatar"
+          style={[avatarSize, styles.avatar, styles.image]}
+        />
+      ) : (
+        <View style={[avatarSize, styles.avatar, styles.noImage]} />
+      )}
+      <View>
+        <Button
+          title={uploading ? 'Uploading ...' : 'Upload'}
+          onPress={uploadAvatar}
+          disabled={uploading}
+        />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  avatar: {
+    borderRadius: 5,
+    overflow: 'hidden',
+    maxWidth: '100%',
+  },
+  image: {
+    objectFit: 'cover',
+    paddingTop: 0,
+  },
+  noImage: {
+    backgroundColor: '#333',
+    borderRadius: 5,
+  },
+})
