6c50ddb285d11032f81e30441612788addeda7ea
Julia Siqueira <jusiqueira@davidson.edu>
2026-04-10 12:45:52 -0400

    Added app.jsx back with no errors

--- App.jsx
+++ App.jsx
@@ -1,3 +1,58 @@
+import 'react-native-url-polyfill/auto'
+import { useState, useEffect } from 'react'
+import { supabase } from './supabase'
+import Auth from './components/Auth'
+import Account from './components/Account'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+
+const Stack = createStackNavigator()
+
+export default function App() {
+  const [userId, setUserId] = useState<string | null>(null)
+  const [email, setEmail] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    supabase.auth.getClaims().then(({ data: { claims } }) => {
+      if (claims) {
+        setUserId(claims.sub)
+        setEmail(claims.email)
+      }
+    })
+
+    supabase.auth.onAuthStateChange(async (_event, _session) => {
+      const {
+        data: { claims },
+      } = await supabase.auth.getClaims()
+      if (claims) {
+        setUserId(claims.sub)
+        setEmail(claims.email)
+      } else {
+        setUserId(null)
+        setEmail(undefined)
+      }
+    })
+  }, [])
+
+  return ( 
+      <NavigationContainer> 
+          <Stack.Navigator>
+            {userId ? (
+              <>
+              <Stack.Screen name="Dashboard" component={DashboardScreen} />
+              <Stack.Screen name="Account"> 
+                {() => <Account key={userId} userId={userId} email={email} />}
+              </Stack.Screen>
+              </>
+            ) : (
+              <Stack.Screen name="Login" component={Auth} />
+            )}
+          </Stack.Navigator>
+          </NavigationContainer>
+  )
+}
+
+{/* 
 import { NavigationContainer } from '@react-navigation/native' //lines 1-14 from Claude
 import { createStackNavigator } from '@react-navigation/stack'
 import DashboardScreen from './screens/DashboardScreen'
@@ -16,4 +71,6 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>
   )
-}
+} 
+
+*/} 
